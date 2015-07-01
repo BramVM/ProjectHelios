159d3725cb2d7d7f47e6f2bd1db0da020e769b67
Nick Mingneau <nick.mingneau@progreso.be>
2015-07-01 17:59:41 +0200

    returns pretty color object

--- jsonImporter.js
+++ jsonImporter.js
@@ -1,20 +1,17 @@
 var spawnMesh = function(voxels){
-	var voxelScale = 30;
 
+	var voxelScale = 30;
 	var geometry = new THREE.BoxGeometry( voxelScale, voxelScale, voxelScale );
 	var material = new THREE.MeshLambertMaterial();
-
 	var voxelArray = [];
-
 	var combined = new THREE.Geometry();
 
 	for(var i=0; i<voxels.length; i++){
 		voxelArray[i] = new THREE.Mesh(geometry, material);
 		voxelArray[i].position.set(voxels[i].position[0]*voxelScale , voxels[i].position[1]*voxelScale , 0);
-
 		THREE.GeometryUtils.merge( combined, voxelArray[i] );
 	}
-
+	
 	var mesh = new THREE.Mesh( combined );
 	return(mesh);
 }
--- spawnMesh.js
+++ spawnMesh.js
@@ -1,20 +1,20 @@
 var spawnMesh = function(voxels){
+
 	var voxelScale = 30;
-
 	var geometry = new THREE.BoxGeometry( voxelScale, voxelScale, voxelScale );
-	var material = new THREE.MeshLambertMaterial();
-
 	var voxelArray = [];
-
-	var combined = new THREE.Geometry();
+	var materialArray = [];
+	var combined = new THREE.Object3D();
 
 	for(var i=0; i<voxels.length; i++){
-		voxelArray[i] = new THREE.Mesh(geometry, material);
+
+		var curColor = parseInt("0x" + voxels[i].color);
+		materialArray[i] = new THREE.MeshLambertMaterial({ color: curColor });
+
+		voxelArray[i] = new THREE.Mesh(geometry, materialArray[i]);
 		voxelArray[i].position.set(voxels[i].position[0]*voxelScale , voxels[i].position[1]*voxelScale , 0);
 
-		THREE.GeometryUtils.merge( combined, voxelArray[i] );
+		combined.add(voxelArray[i]);
 	}
-
-	var mesh = new THREE.Mesh( combined );
-	return(mesh);
+	return(combined);
 }
