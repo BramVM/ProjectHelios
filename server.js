499a2a1a27f82be21b896bc7b025fcb6fcf1546f
BramVM <vanmensel.bram@gmail.com>
2015-07-04 00:54:32 +0200

    serverForOpenshift

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.use('/', express.static(__dirname));
-
-app.listen(30000, function() { console.log('listening')});
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "174.129.184.45";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+server.listen( port, ipaddress, function() {
+    console.log((new DATE()) + ' Server is listening on port 8080');
+});
