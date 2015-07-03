8b96542f10094b690ff934e657cf5560232ee17e
BramVM <vanmensel.bram@gmail.com>
2015-07-04 01:28:04 +0200

    lastServerFix

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen( port, ipaddress, function() {
-    console.log((new DATE()) + ' Server is listening on port 8080');
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
