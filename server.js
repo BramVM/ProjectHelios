cae62e81ce2a31a7d0ea88628c3b66019e5fc249
BramVM <vanmensel.bram@gmail.com>
2015-07-04 01:14:44 +0200

    serverChanges

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "174.129.184.45";
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "174.129.184.45";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.write("Welcome to Node.js on OpenShift!\n\n");
+      response.end("Thanks for visiting us! \n");
+});
+
 server.listen( port, ipaddress, function() {
     console.log((new DATE()) + ' Server is listening on port 8080');
 });
+
+wss = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  console.log("New connection");
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
