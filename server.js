89bf990b93688f476296e8def48b6c7a9e8dd4a3
Bram Van Mensel <vanmensel.bram@gmail.com>
2015-10-19 11:20:13 +0200

    updated server

--- server.js
+++ server.js
@@ -27,14 +27,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function() {
