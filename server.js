4b8614b6990eb71551148074122fab2c782bc82e
Bram Van Mensel <vanmensel.bram@gmail.com>
2015-10-19 11:16:47 +0200

    server changed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// to surve localy use http://localhost:8080/
+/*// to surve localy use http://localhost:8080/
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -20,4 +20,23 @@ function runServer (){
 	});
 }
 
-exec("browserify main.js -o bundle.js",runServer());
+exec("browserify main.js -o bundle.js",runServer());*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
