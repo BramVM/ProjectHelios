9b5c50d7c332cb17716ea09a17ca40833c247574
Bram Van Mensel <vanmensel.bram@gmail.com>
2015-10-19 18:15:03 +0200

    deleted old server code

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-/*// to surve localy use http://localhost:8080/
-var express = require('express');
-var app = express();
-var path = require('path');
-var exec = require("child_process").exec;
-//var compressor = require('node-minify');
-
-app.use(express.static(__dirname + '/'));
-
-app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname + 'index.html'));
-});
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP ||'127.0.0.1'
-
-function runServer (){
-	app.listen(server_port, server_ip_address, function () {
-	  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
-	});
-}
-
-exec("browserify main.js -o bundle.js",runServer());*/
-
 var express = require('express');
 var app = express();
 var exec = require("child_process").exec;+
